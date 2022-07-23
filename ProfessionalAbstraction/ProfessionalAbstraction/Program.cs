ca70cbb142b11ae53b703da77dca3096e3d2c635
Serhat-Koroglu <105944482+Serhat-Koroglu@users.noreply.github.com>
2022-07-23 12:28:51 +0300

    ProfessionalAbstraction
    ProfessionalAbstraction - Logger

---
+++ ProfessionalAbstraction/ProfessionalAbstraction/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalAbstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new FileLogger();
+            //or
+            customerManager.Logger = new SmsLogger();
+
+            customerManager.Add();
+            Console.ReadLine();
+        }
+    }
+    class CustomerManager
+    {
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            Logger.Log();
+            Console.WriteLine("Customer Added!");
+        }
+    }
+    class SmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Sms!");
+        }
+    }
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file!");
+        }
+    }
+    interface ILogger
+    {
+        void Log();
+    }
+}
